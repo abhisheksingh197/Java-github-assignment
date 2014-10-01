ef2e1c44acf8827f07dde4e74bb3e3c7b354e1c0
Sripathi Krishnan <Sripathi.Krishnan@gmail.com>
2014-10-01 12:48:39 +0530

    Adding a URL to handle search

--- src/main/java/com/hashedin/artcollective/controller/WebApplicationController.java
+++ src/main/java/com/hashedin/artcollective/controller/WebApplicationController.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +28,16 @@ public final class WebApplicationController {
 		model.put("time", new Date());
 		model.put("message", "Hello to Secure World!");
 		
+		return new ModelAndView("index", model);
+	}
+	
+	@RequestMapping("/proxy/search")
+	public ModelAndView search(HttpServletResponse response) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("time", new Date());
+		model.put("message", "Hello to Secure World!");
+		
+		response.setContentType("application/liquid");
 		return new ModelAndView("index", model);
 	}
 }
